2faf5d0b6125b477c58ffb9d2e1100c6ecccf29a
mononomori <miguel.a.cannuli@gmail.com>
2024-05-05 22:36:46 -0700

    initial branch commit

--- git.nix
+++ git.nix
@@ -1,7 +1,7 @@
 {config, lib, pkgs, ...}:
 
 {
-  environment.systemPackages = [ pkgs.gitFull ];
+  environment.systemPackages = [pkgs.gitFull];
   programs.git.enable = true;
   programs.git.config.user.name = "mononomori";
   programs.git.config.user.email = "miguel.a.cannuli@gmail.com";
