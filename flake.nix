8958f212c98a13a2380415e504c9e42ceada5284
mononomori <miguel.a.cannuli@gmail.com>
2024-06-08 23:28:20 -0700

    chore: cleaned up unused declared options and deprecated features

--- flake.nix
+++ flake.nix
@@ -5,53 +5,64 @@
     # Default to the nixos-unstable branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Latest stable branch of nixpkgs, used for version rollback
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
-
     # Hardware pkgs
     nixos-hardware.url = "github:NixOs/nixos-hardware/master";
-
+    # Enable home-manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Latest version of Hyprland
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, nixos-hardware, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, hyprland, ... }@inputs:
     let
       # ---- System Settings ---- #
       system = "x86_64-linux";
       hostname = "YoRNix";
       # configure lib
-      lib = nixpkgs.lib; 
-      overlay-stable = final: prev: {
-        stable = import nixpkgs-stable {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            permittedInsecurePackages = [
-              "electron-28.2.10"
-            ];
-          };
-        }; 
-      }; 
-    in
-    {
-      nixosConfigurations.YoRNix = lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit hostname;
-            inherit inputs;
-          };
-          modules = [ 
-            ({ config, pkgs, ...}: { nixpkgs.overlays = [ overlay-stable ]; })
-            nixos-hardware.nixosModules.framework-13-7040-amd
-            ./configuration.nix
-            inputs.home-manager.nixosModules.default
-            ./display-manager.nix
-            ./git.nix
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          permittedInsecurePackages = [
+            "electron-28.2.10"
           ];
         };
-
+      };
+    in
+    {
+      nixosConfigurations.YoRNix = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit hostname;
+          inherit inputs;
+          inherit system;
+        };
+        modules = [ 
+          nixos-hardware.nixosModules.framework-13-7040-amd
+          ./configuration.nix
+          inputs.home-manager.nixosModules.default
+          inputs.hyprland.nixosModules.default
+          ./display-manager.nix
+          ./git.nix
+        ];
+      };
+      homeConfigurations = {
+        "_2b@YoRNix" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          inherit hostname;
+          inherit system;
+          inherit inputs;
+          modules = [
+            ./home.nix
+          ];
+        };
+      };
     };
 }
