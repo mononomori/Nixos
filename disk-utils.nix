5bea84ef869fcb691c23749f4ce89ca1a9e23bf5
mononomori <miguel.a.cannuli@gmail.com>
2024-12-06 16:59:24 -0800

    feat: enabled fstrim with a weekly interval to reclaim unused ssd space

--- disk-utils.nix
+++ disk-utils.nix
@@ -1,24 +1,30 @@
 { config, pkgs, lib, inputs, diskusers, ...}:
 {
 
-
   users = {
     users = lib.genAttrs diskusers (name: {
       extraGroups = [ "disk" ];
     });
   };
+
   environment.systemPackages = with pkgs; [
     #### Disk Management Tools:
     ntfs3g
     udisks
     udiskie
     usbutils
-
     #### CLI Utilities:
     parted
   ];
 
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
+  services = {
+    gvfs.enable = true;
+    udisks2.enable = true;
+    fstrim = {
+      enable = true;
+      interval = weekly;
+    };
+  };
+
 
 }
