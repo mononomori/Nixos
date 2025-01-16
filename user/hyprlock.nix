1a051c66c7761931d40fbccde1c8dea93de6718f
mononomori <miguel.a.cannuli@gmail.com>
2025-01-16 14:28:00 -0800

    feat: finalized current implementations styling

--- user/hyprlock.nix
+++ user/hyprlock.nix
@@ -7,8 +7,6 @@
     hyprlock
   ];
 
-
-
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -27,7 +25,7 @@
           blur_passes = 3;
           noise = 0.0117;
           contrast = 1.3000;
-          brightness = 1.0;
+          brightness = 0.75;
           vibrancy = 0.2100;
           vibrancy_darkness = 0.05;
         }
@@ -36,9 +34,13 @@
       label = [
         {
           text = ''cmd[update:100] echo "<span font-family='Fira Code' foreground='##e4c9af'>$(date +'%H, %M %S')</span>"'';
-          color = "rgba(242, 243, 244, 0.75)";
+          color = "rgba(228,201,175,1.0)";
           font_size = 95;
           font_family = "JetBrains Mono";
+          shadow_passes = 2;
+          shadow_size	= 2;
+          shadow_color = "rgba(105, 57, 69, 1.0)";
+          shadow_boost = 1.0;
           position = "2100, 750";
           halign = "left";
           valign = "center";
@@ -48,6 +50,10 @@
           color = "rgba(242, 243, 244, 0.75)";
           font_size = 44;
           font_family = "JetBrains Mono";
+          shadow_passes = 2;
+          shadow_size	= 2;
+          shadow_color = "rgba(105, 57, 69, 1.0)";
+          shadow_boost = 1.0;
           position = "2100, 650";
           halign = "left";
           valign = "center";
@@ -57,11 +63,16 @@
           color = "rgba(242, 243, 244, 0.75)";
           font_size = 44;
           font_family = "Iosevka Term";
+          shadow_passes = 2;
+          shadow_size	= 2;
+          shadow_color = "rgba(105, 57, 69, 1.0)";
+          shadow_boost = 1.0;
           position = "0, 50";
           halign = "center";
           valign = "center";
         }
       ];
+
 
       input-field = [
         {
@@ -73,11 +84,11 @@
           dots_center = true;
           dots_text_format = "の";
           outer_color = "rgba(0, 0, 0, 0)";
-          inner_color = "rgba(0, 0, 0, 0.2)";
+          inner_color = "rgba(210, 115, 138, 0.2)";
           font_color = "rgb(228, 201, 175)";
           fade_on_empty = false;
           rounding = -1;
-          check_color = "rgb(30, 107, 204)";
+          check_color = "rgba(114, 100, 87, 1.0)";
           placeholder_text = ''<span font-family="Fira Code" foreground="##d2738a">Let's all love</span><span font-family="Fira Code" foreground="##e4c9af"> $USER!</span>'';
           
           hide_input = false;
