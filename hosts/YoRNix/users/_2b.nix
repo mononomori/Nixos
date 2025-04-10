f885476c319880dcb3ca1b53679770d92bbf3e59
mononomori <miguel.a.cannuli@gmail.com>
2025-04-09 23:47:47 -0700

    chore: cleaned up a ton of default config gen comments

--- hosts/YoRNix/users/_2b.nix
+++ hosts/YoRNix/users/_2b.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ../../../modules/home-manager/desktop/hyprland.nix
+    ../../../modules/home-manager/desktop/hyprland-env.nix
     ../../../modules/home-manager/desktop/hypridle.nix
     ../../../modules/home-manager/desktop/hyprlock.nix
     ../../../modules/home-manager/fuzzel.nix
@@ -11,13 +12,11 @@
     ../../../modules/home-manager/yazi.nix
   ];
   
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # Home Manager needs a bit of information about you and the paths it should manage.
   home.username = "_2b";
   home.homeDirectory = "/home/_2b";
   home.sessionPath = [
   ];
-
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -28,48 +27,19 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
 
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
+    # Symlink my power menu script so it's runnable as a command and shows up in dmenu/fuzzel
+    ".local/bin/power-menu".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/modules/home-manager/scripts/fuzzel-power-menu.sh";
 
-    ".local/bin/power-menu".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/user/scripts/fuzzel-power-menu.sh";
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
- 
+  # Configure user specific desktop settings
+
+  # Cursor
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
@@ -79,19 +49,18 @@
   };
 
 
-  #Gtk
+  # GTK theming
   gtk = {
     enable = true;
+
     font = {
-      # package = pkgs.nerd-fonts.mononoki;
-      # name = "Mononoki Nerd Font Regular";
-      # size = 10;
       name = "IosevkaB Extended Medium";
       size = 12;
     };
+
     theme = {
-      name = "Juno-mirage";
-      package = pkgs.juno-theme;
+      name = "Orchis-Pink-Dark";
+      package = pkgs.orchis-theme;
     };
 
     iconTheme = {
@@ -110,37 +79,15 @@
     };
 
     gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-        gtk-cursor-theme-name=Bibata-Modern-Classic
-      '';
+      "gtk-application-prefer-dark-theme" = "1";
+      "gtk-cursor-theme-name" = "Bibata-Modern-Classic";
     };
 
   };
 
-
-
-
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/_2b/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-
-    # EDITOR = "emacs";
+  
   };
-  # Let Home Manager install and manage itself.
+
   programs.home-manager.enable = true;
 }
