67c2fe972765f566e1f3006e9214e248b51ef838
mononomori <miguel.a.cannuli@gmail.com>
2025-05-15 12:17:53 -0700

    feat: changed hypridle to only dim brightness if brightness level is already above 20%, also removed deprecated settings

--- modules/home-manager/desktop/hypridle.nix
+++ modules/home-manager/desktop/hypridle.nix
@@ -10,8 +10,6 @@
     settings = {
       general = [ 
         {
-          mouse_move_enables_dpms = true;                # enables mouse movement treated as activity.       
-          key_press_enables_dpms = true;                 # enables key press treated as activity.
           lock_cmd = "pgrep hyprlock || hyprlock";       # avoids starting multiple hyprlock instances.
           before_sleep_cmd = "loginctl lock-session";    # lock before suspend.
           after_sleep_cmd = "hyprctl dispatch dpms on";  # to avoid having to press a key twice to turn on the display.
@@ -20,9 +18,9 @@
         # set monitor backlight to minimum when timeout has passed, restore monitor backlight when activity detected.
       listener = [ 
         {
-          timeout = 150;                                
-          on-timeout = "brightnessctl -s set 50%-";       
-          on-resume = "brightnessctl -r";               
+          timeout = 150;
+          on-timeout = "brightnessctl -s; curr=$(brightnessctl g); max=$(brightnessctl m); perc=$(( 100 * curr / max )); if [ \"$perc\" -gt 20 ]; then brightnessctl set 20%; fi";
+          on-resume = "brightnessctl -r";
         }
         # keyboard backlight when timout has passed, screen on when activity detected.
         { 
