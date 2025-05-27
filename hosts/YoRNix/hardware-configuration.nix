3610195601c7528bcf4ec31bfc08010386720822
mononomori <miguel.a.cannuli@gmail.com>
2025-05-27 12:37:07 -0700

    fix: moved powerprofile setting into it's appropriate power-management.nix module

--- hosts/YoRNix/hardware-configuration.nix
+++ hosts/YoRNix/hardware-configuration.nix
@@ -13,7 +13,6 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
   boot.kernelParams = [ "amd_pstate=guided" ];
-  powerManagement.cpuFreqGovernor = "schedutil";
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/e0398e90-0589-4c4a-af71-65041460ac6f";
