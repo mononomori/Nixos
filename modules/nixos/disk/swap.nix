c508e4d6de02eb5a41cd20460ede63ee1e2f6e95
mononomori <miguel.a.cannuli@gmail.com>
2026-03-25 14:00:04 -0700

    fix: added the boot options necessary to use zswap.compressor = lz4

--- modules/nixos/disk/swap.nix
+++ modules/nixos/disk/swap.nix
@@ -1,12 +1,19 @@
 { config, pkgs, lib, inputs, ... }:
 {
 
-  boot.kernelParams = [ 
-    "zswap.enabled=1"
-    "zswap.max_pool_percent=20"
-    "zswap.compressor=lz4"
-    "zswap.shrinker_enabled=1"
-  ];
+  boot = {
+    # Both needed so zswap can use lz4 on boot
+    initrd = {
+      systemd.enable = true;
+      kernelModules = [ "lz4" ];
+    };
+    kernelParams = [ 
+      "zswap.enabled=1"
+      "zswap.max_pool_percent=20"
+      "zswap.compressor=lz4"
+      "zswap.shrinker_enabled=1"
+    ];
+  };
 
   swapDevices = [
     { device = "/dev/disk/by-uuid/9db274bb-c23e-4616-a0b0-18c7da0660ad"; }
