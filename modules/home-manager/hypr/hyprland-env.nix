977cc1ff9059f5e4afc2140ceb0313f6d1275efc
mononomori <miguel.a.cannuli@gmail.com>
2026-03-28 15:32:50 -0700

    fix: systemd was reading xcb as a command because of the semi-colon, wrapping it in quotes fixed it

--- modules/home-manager/hypr/hyprland-env.nix
+++ modules/home-manager/hypr/hyprland-env.nix
@@ -12,7 +12,7 @@ in
     export GDK_DPI_SCALE=1
     export GDK_SCALE=1
 
-    export QT_QPA_PLATFORM=wayland;xcb
+    export QT_QPA_PLATFORM="wayland;xcb"
     export QT_AUTO_SCREEN_SCALE_FACTOR=1
     export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
     export QT_QPA_PLATFORMTHEME=qt5ct
