2e360f1ef0be3ed4c813c33f3e302314479ffcf0
mononomori <miguel.a.cannuli@gmail.com>
2026-03-28 15:33:34 -0700

    fix: added ownership mask to boot as it was a security vulnerability before

--- hosts/YoRNix/hardware-configuration.nix
+++ hosts/YoRNix/hardware-configuration.nix
@@ -10,6 +10,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/7BA5-01F5";
       fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
     };
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
