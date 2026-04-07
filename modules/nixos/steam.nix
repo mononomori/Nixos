702766070f1a5d796ec71ba98c148a676f226d4f
mononomori <miguel.a.cannuli@gmail.com>
2026-04-07 13:50:04 -0700

    feat: added ntsync emulation for windows sync primitives

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -9,6 +9,18 @@
       steam
     ;
   };
+
+  # ntsync emulates Windows sync primitives for better windows gaming performance.
+
+  boot.kernelModules = [ "ntsync" ]; 
+
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "ntsync-udev-rules";
+      text = ''KERNEL=="ntsync", MODE="0660", TAG+="uaccess"'';
+      destination = "/etc/udev/rules.d/70-ntsync.rules";
+    })
+  ];
   
   programs = {
     steam = {
