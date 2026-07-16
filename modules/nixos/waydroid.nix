985b8cbe418aadbe0dbc2bd282785a8ae9656b36
mononomori <miguel.a.cannuli@gmail.com>
2026-07-15 18:22:09 -0700

    feat: added waydroid nix module with google apps

---
+++ modules/nixos/waydroid.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
+  virtualisation.waydroid.enable = true;
+  virtualisation.waydroid.package = pkgs.waydroid-nftables;
+
+  # Enable clipboard sharing
+  environment.systemPackages = [ pkgs.wl-clipboard ];
+
+  # Persist these android props into waydroid_base.prop before every container start.
+  systemd.services.waydroid-container.serviceConfig.ExecStartPre = lib.mkAfter [
+    (pkgs.writeShellScript "waydroid-persist-props" ''
+      set -eu
+      PROP_FILE=/var/lib/waydroid/waydroid_base.prop
+      set_prop() {
+        if [ -f "$PROP_FILE" ]; then
+          ${pkgs.gnused}/bin/sed -i "/^$1=/d" "$PROP_FILE"
+          echo "$1=$2" >> "$PROP_FILE"
+        fi
+      }
+      set_prop persist.waydroid.fake_touch '*'
+      set_prop persist.waydroid.width 1440
+      set_prop persist.waydroid.height 900
+    '')
+  ];
+}
